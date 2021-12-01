3bfd53efee0ca67309d80efaae91959b3fe2ac19
SJD-BYUI <86030230+SJD-BYUI@users.noreply.github.com>
2021-12-01 09:04:06 -0700

    Removed Food
    removed all the food from the game

--- snake-complete/snake-complete/snake/__main__.py
+++ snake-complete/snake-complete/snake/__main__.py
@@ -1,9 +1,10 @@
 import constants
 
 from game.casting.cast import Cast
-from game.casting.food import Food
+
 from game.casting.score import Score
 from game.casting.snake import Snake
+#from game.casting.food import Food
 from game.scripting.script import Script
 from game.scripting.control_actors_action import ControlActorsAction
 from game.scripting.move_actors_action import MoveActorsAction
@@ -20,7 +21,7 @@ def main():
     
     # create the cast
     cast = Cast()
-    cast.add_actor("foods", Food())
+    # cast.add_actor("foods", Food())
     cast.add_actor("snakes", Snake())
     cast.add_actor("scores", Score())
    
--- snake-complete/snake-complete/snake/game/scripting/draw_actors_action.py
+++ snake-complete/snake-complete/snake/game/scripting/draw_actors_action.py
@@ -27,13 +27,13 @@ class DrawActorsAction(Action):
             script (Script): The script of Actions in the game.
         """
         score = cast.get_first_actor("scores")
-        food = cast.get_first_actor("foods")
+        # food = cast.get_first_actor("foods")
         snake = cast.get_first_actor("snakes")
         segments = snake.get_segments()
         messages = cast.get_actors("messages")
 
         self._video_service.clear_buffer()
-        self._video_service.draw_actor(food)
+        # self._video_service.draw_actor(food)
         self._video_service.draw_actors(segments)
         self._video_service.draw_actor(score)
         self._video_service.draw_actors(messages, True)
